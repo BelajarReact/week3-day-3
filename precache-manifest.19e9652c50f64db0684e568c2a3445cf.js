33ffb91f8a2e4376726a5b82a2288fe0936d6673
Hadie Danker <hadie87@gmail.com>
2020-07-02 02:32:56 +0700

    Updates

---
+++ precache-manifest.19e9652c50f64db0684e568c2a3445cf.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1e583cc1e51c15c348be6e3e6e775bbd",
+    "url": "/week3-day-3/index.html"
+  },
+  {
+    "revision": "fe6a77689cc613f90586",
+    "url": "/week3-day-3/static/css/main.9f1707e8.chunk.css"
+  },
+  {
+    "revision": "a10b8a6266d6c993e0c1",
+    "url": "/week3-day-3/static/js/2.2e1fb5e5.chunk.js"
+  },
+  {
+    "revision": "fe6a77689cc613f90586",
+    "url": "/week3-day-3/static/js/main.8efcda28.chunk.js"
+  },
+  {
+    "revision": "c03d27dc9fbbe97a8e7d",
+    "url": "/week3-day-3/static/js/runtime~main.a8c21186.js"
+  }
+]);
